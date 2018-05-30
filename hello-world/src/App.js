7126beabc75f0d169b57a1c3a8c807c2ea4bdea3
john andrey <andrey_201015@hotmail.com>
2018-05-29 21:31:04 -0500

    Terminando detalles

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,6 +5,16 @@ import platzi from './platzi.png'
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 console.log(logo)
 class App extends Component {
+
+  state = {
+    logo: logo,
+  }
+  onClick() {
+    this.setState({
+      logo: platzi,
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,9 +24,15 @@ class App extends Component {
             transitionEnterTimeout={1000}
             transitionLeaveTimeout={1000}
           >
-            <img src={logo} className="App-logo" alt="logo" />
+            <img
+              key={this.state.logo}
+              src={this.state.logo}
+              className="App-logo"
+              alt="logo"
+            />
           </CSSTransitionGroup>
           <h1 className="App-title">Welcome to React</h1>
+          <button onClick={this.onClick.bind(this)}>Click me!</button>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
